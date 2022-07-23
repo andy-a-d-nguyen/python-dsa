803c96f3b76a6937bfc7d2e3b4d00b5399ad75ab
Andy Nguyen <a.d.nguyen.andy@gmail.com>
2022-07-23 16:47:22 -0700

    Finished Linked List

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, value):
+    def __init__(self, value: object) -> object:
         self.value = value
         self.next = None
 
@@ -21,22 +21,120 @@ class LinkedList:
         self.head = None
 
     def get(self, index):
-        pass
+        if self.head is None:
+            raise IndexError
+        count = 0
+        current_node = self.head
+        # if we check the next node instead of the current node
+        # when we get to the last node
+        # because the last node's next node is None
+        # while loop terminates
+        # and last node is not checked
+        while current_node is not None:
+            if count == index:
+                return current_node.value
+            count += 1
+            current_node = current_node.next
+        raise IndexError
 
-    def insert(self, index, value):
-        pass
+    def insert(self, index, node: Node):
+        if self.head is None:
+            if index == 0:
+                self.head = node
+            else:
+                raise IndexError
+        else:
+            count = 0
+            current_node = self.head
+            next_node = self.head.next
+            if index == 0:
+                self.head = node
+                node.next = current_node
+                current_node.next = next_node
+                return
+            # We want to check if next node is None here
+            # because we can't keep track of previous node
+            # so this check is the same as assuming next node is current node
+            # and current node is previous node
+            while next_node is not None:
+                count += 1
+                if count == index:
+                    current_node.next = node
+                    node.next = next_node
+                    break
+                else:
+                    current_node = current_node.next
+                    next_node = next_node.next
+            if count < index:
+                raise IndexError
 
     def is_empty(self):
-        pass
+        return self.head is None
 
     def remove(self, index):
-        pass
+        if self.head is not None:
+            if index == 0:
+                current_node = self.head
+                self.head = None
+                return current_node.value
+            elif index > 0:
+                count = 0
+                current_node = self.head
+                next_node = self.head.next
+                while next_node is not None:
+                    count += 1
+                    if count == index:
+                        current_node.next = next_node.next
+                        return next_node.value
+                    else:
+                        current_node = current_node.next
+                        next_node = next_node.next
+            else:
+                raise IndexError
+        else:
+            raise IndexError
 
-    def set(self, index, value):
-        pass
+    def set(self, index, node: Node):
+        if self.head is None:
+            if index == 0:
+                self.head = node
+            else:
+                raise IndexError
+        else:
+            current_node = self.head
+            next_node = self.head.next
+            if index == 0:
+                self.head = node
+                # if we assign current_node instead
+                # the operation becomes an insert, not replace
+                node.next = next_node
+            elif index > 0:
+                count = 0
+                while next_node is not None:
+                    count += 1
+                    if count == index:
+                        current_node.next = node
+                        # if we point to next_node instead of next_node.next
+                        # this operation becomes an insert, not replace
+                        node.next = next_node.next
+                        break
+                    current_node = current_node.next
+                    next_node = next_node.next
+                if count < index:
+                    raise IndexError
+            else:
+                raise IndexError
 
     def size(self):
-        pass
+        if self.head is None:
+            return 0
+        else:
+            count = 1
+            current_node = self.head
+            while current_node.next is not None:
+                current_node = current_node.next
+                count += 1
+            return count
 
     def to_string(self):
         return str(self.head)
--- linked_list/test_linked_list.py
+++ linked_list/test_linked_list.py
@@ -1,3 +1,5 @@
+import pytest
+
 from linked_list.linked_list import LinkedList
 from linked_list.linked_list import Node
 
@@ -20,9 +22,11 @@ def test_add():
     n2 = Node(2)
     ll.add(n2)
     assert ll.head.next == n2
+    assert ll.head.next.value == n2.value
     n3 = Node(3)
     ll.add(n3)
     assert n2.next == n3
+    assert n2.next.value == n3.value
 
 
 def test_clear():
@@ -31,3 +35,78 @@ def test_clear():
     ll.add(n1)
     ll.clear()
     assert ll.to_string() == "None"
+
+
+def test_get():
+    ll = LinkedList()
+    ll.add(Node(1))
+    assert ll.get(0) == 1
+    ll.add(Node(2))
+    assert ll.get(1) == 2
+    ll.add(Node(3))
+    assert ll.get(2) == 3
+    with pytest.raises(IndexError):
+        ll.get(10)
+
+
+def test_insert():
+    ll = LinkedList()
+    ll.insert(0, Node(1))  # [1]
+    assert ll.get(0) == 1
+    ll.add(Node(2))  # [1, 2]
+    assert ll.get(0) == 1
+    assert ll.get(1) == 2
+    ll.insert(0, Node(4))  # [4, 1, 2]
+    assert ll.get(0) == 4
+    assert ll.get(1) == 1
+    assert ll.get(2) == 2
+    ll.insert(1, Node(3))  # [4, 3, 1, 2]
+    assert ll.get(1) == 3
+    with pytest.raises(IndexError):
+        ll.insert(10, Node(10))
+
+
+def test_is_empty():
+    ll = LinkedList()
+    assert ll.is_empty() is True
+    ll.add(Node(1))
+    assert ll.is_empty() is False
+
+
+def test_remove():
+    ll = LinkedList()
+    ll.add(Node(1))
+    assert ll.remove(0) == 1
+    with pytest.raises(IndexError):
+        ll.remove(0)
+    ll.add(Node(2))
+    ll.add(Node(3))
+    ll.add(Node(4))
+    assert ll.remove(1) == 3
+
+
+def test_set():
+    ll = LinkedList()
+    with pytest.raises(IndexError):
+        ll.set(1, Node(4))
+    ll.set(0, Node(1))  # [1]
+    assert ll.get(0) == 1
+    ll.add(Node(2))  # [1, 2]
+    assert ll.get(1) == 2
+    ll.set(0, Node(5))  # [5, 2]
+    assert ll.get(0) == 5
+    assert ll.get(1) == 2
+    ll.add(Node(3))  # [5, 2, 3]
+    ll.set(1, Node(4))  # [5, 4, 3]
+    assert ll.get(1) == 4
+    with pytest.raises(IndexError):
+        ll.set(10, Node(10))
+
+
+def test_size():
+    ll = LinkedList()
+    assert ll.size() == 0
+    ll.add(Node(1))
+    assert ll.size() == 1
+    ll.add(Node(2))
+    assert ll.size() == 2
