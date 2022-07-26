026ece8406f787fa62827516553562da3ee583de
Andy Nguyen <a.d.nguyen.andy@gmail.com>
2022-07-26 08:10:58 -0700

    Finish Queue

--- queue/queue.py
+++ queue/queue.py
@@ -7,6 +7,58 @@ class Node:
 class LinkedList:
     def __init__(self, head=None):
         self.head = head
+        self.length = 0 if self.head is None else 1
+
+    def add(self, value):
+        n = Node(value)
+        if self.head is None:
+            self.head = n
+        else:
+            current_node = self.head
+            while current_node.next is not None:
+                current_node = current_node.next
+            current_node.next = n
+        self.length += 1
+
+    def get(self, index):
+        if self.head is None:
+            raise IndexError('List is empty')
+        else:
+            count = 0
+            current_node = self.head
+            while current_node is not None:
+                if count == index:
+                    return current_node.value
+                else:
+                    count += 1
+                    current_node = current_node.next
+
+    def remove(self, index):
+        if self.head is None:
+            raise IndexError('List is empty')
+        else:
+            count = 0
+            current_node = self.head
+            next_node = self.head.next
+            if index == 0:
+                self.length -= 1
+                self.head = next_node
+                return current_node.value
+            else:
+                while next_node is not None:
+                    count += 1
+                    if count == index:
+                        current_node.next = next_node.next
+                        self.length -= 1
+                        return next_node.value
+                    else:
+                        current_node = current_node.next
+                        next_node = next_node.next
+                if index > count:
+                    raise IndexError('Index out of range')
+
+    def is_empty(self):
+        return self.head is None
 
     def __str__(self):
         return str(self.head)
@@ -16,8 +68,49 @@ class Queue:
     def __init__(self):
         self.queue = LinkedList()
 
+    def dequeue(self):
+        if self.queue.is_empty():
+            raise IndexError('Queue is empty')
+        else:
+            return self.queue.remove(0)
+
     def enqueue(self, node):
-        pass
+        self.queue.add(node.value)
+
+    def is_empty(self):
+        return self.queue.is_empty()
+
+    def peek(self):
+        if self.queue.is_empty():
+            raise IndexError('Queue is empty')
+        else:
+            return self.queue.get(0)
+
+    def size(self):
+        return self.queue.length
 
     def __str__(self):
         return str(self.queue)
+
+
+def stutter(queue):
+    result = Queue()
+
+    while queue.size() > 0:
+        current_value = queue.dequeue()
+        result.enqueue(Node(current_value))
+        result.enqueue(Node(current_value))
+
+    return result
+
+
+def mirror(queue):
+    values = []
+    size = queue.size()
+    for _ in range(size):
+        current_value = queue.dequeue()
+        queue.enqueue(Node(current_value))
+        values.append(current_value)
+    for value in reversed(values):
+        queue.enqueue(Node(value))
+    return queue
--- queue/test_queue.py
+++ queue/test_queue.py
@@ -1,6 +1,39 @@
+import pytest
+
 from queue.queue import Queue
 from queue.queue import LinkedList
 from queue.queue import Node
+from queue.queue import stutter
+from queue.queue import mirror
+
+
+def test_add():
+    ll = LinkedList()
+    ll.add(1)
+    assert ll.head.value == 1
+    ll.add(2)
+    assert ll.head.next.value == 2
+
+
+def test_get():
+    ll = LinkedList()
+    with pytest.raises(IndexError) as message:
+        ll.get(0)
+    assert message.value.args[0] == 'List is empty'
+    ll.add(1)
+    assert ll.get(0) == 1
+    ll.add(2)
+    assert ll.get(1) == 2
+
+
+def test_remove():
+    ll = LinkedList()
+    ll.add(1)
+    ll.add(2)
+    assert ll.remove(0) == 1
+    ll.add(3)
+    ll.add(4)
+    assert ll.remove(2) == 4
 
 
 def test_queue():
@@ -10,4 +43,62 @@ def test_queue():
 
 def test_enqueue():
     q = Queue()
-    assert False
+    q.enqueue(Node(1))
+    assert q.queue.head.value == 1
+
+
+def test_dequeue():
+    q = Queue()
+    with pytest.raises(IndexError) as message:
+        q.dequeue()
+    assert message.value.args[0] == 'Queue is empty'
+    q.enqueue(Node(1))
+    assert q.dequeue() == 1
+
+
+def test_is_empty():
+    q = Queue()
+    assert q.is_empty() is True
+
+
+def test_peek():
+    q = Queue()
+    with pytest.raises(IndexError) as message:
+        q.peek()
+    assert message.value.args[0] == 'Queue is empty'
+    q.enqueue(Node(1))
+    q.enqueue(Node(2))
+    assert q.peek() == 1
+
+
+def test_size():
+    q = Queue()
+    assert q.size() == 0
+    q.enqueue(Node(1))
+    assert q.size() == 1
+
+
+def test_stutter():
+    q = Queue()
+    q.enqueue(Node(1))
+    q.enqueue(Node(2))
+    q.enqueue(Node(3))
+    result = stutter(q)
+    assert result.size() == 6
+    assert result.dequeue() == 1
+    assert result.dequeue() == 1
+
+
+def test_mirror():
+    q = Queue()
+    q.enqueue(Node(1))
+    q.enqueue(Node(2))
+    q.enqueue(Node(3))
+    result = mirror(q)
+    assert result.size() == 6
+    assert result.dequeue() == 1
+    assert result.dequeue() == 2
+    assert result.dequeue() == 3
+    assert result.dequeue() == 3
+    assert result.dequeue() == 2
+    assert result.dequeue() == 1
