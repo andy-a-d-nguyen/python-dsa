c3e8ff22e3bcb694cece8c967fd02ecc5adcd017
Andy Nguyen <a.d.nguyen.andy@gmail.com>
2022-08-03 20:15:47 -0700

    WIP maze

--- backtracking/maze.py
+++ backtracking/maze.py
@@ -1,10 +1,31 @@
 class Maze:
     def __init__(self, row: int, col: int):
         self.maze = []
-        self.row = row
-        self.col = col
+        self.rowSize = 0
+        self.colSize = 0
         for i in range(row):
             self.maze.append([])
+            self.rowSize += 1
         for i in self.maze:
             for j in range(col):
                 i.append('')
+                self.colSize += 1
+        self.colSize /= self.rowSize
+
+    def in_bounds(self, row, col):
+        return row < self.rowSize and col < self.colSize
+
+    def is_marked(self, row, col):
+        return self.maze[row][col] == '*'
+
+    def is_open(self, row, col):
+        return self.maze[row][col] == ''
+
+    def is_tainted(self, row, col):
+        return self.maze[row][col] == '^'
+
+    def is_wall(self, row, col):
+        return self.maze[row][col] == '-'
+
+    def __str__(self):
+        return str(self.maze)
---
+++ backtracking/test_maze.py
@@ -0,0 +1,54 @@
+from backtracking.maze import Maze
+
+
+def test_maze():
+    m = Maze(3, 3)
+    assert str(m) == str([
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+    ])
+    assert m.colSize == 3
+    assert m.rowSize == 3
+    m = Maze(3, 4)
+    assert str(m) == str([
+        ['', '', '', ''],
+        ['', '', '', ''],
+        ['', '', '', ''],
+    ])
+    assert m.colSize == 4
+    assert m.rowSize == 3
+
+
+def test_in_bounds():
+    m = Maze(3, 3)
+    assert m.in_bounds(2, 2) is True
+    assert m.in_bounds(3, 3) is False
+
+
+def test_is_marked():
+    m = Maze(3, 3)
+    m.maze[0][0] = '*'
+    assert m.is_marked(0, 0) is True
+    assert m.is_marked(0, 1) is False
+
+
+def test_is_open():
+    m = Maze(3, 3)
+    m.maze[0][0] = '*'
+    assert m.is_open(0, 0) is False
+    assert m.is_open(0, 1) is True
+
+
+def test_is_tainted():
+    m = Maze(3, 3)
+    m.maze[0][0] = '^'
+    assert m.is_tainted(0, 0) is True
+    assert m.is_tainted(2, 2) is False
+
+
+def test_is_wall():
+    m = Maze(3, 3)
+    m.maze[0][0] = '-'
+    assert m.is_wall(0, 0) is True
+    assert m.is_wall(0, 1) is False
